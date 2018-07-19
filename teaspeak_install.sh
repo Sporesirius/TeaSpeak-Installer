51f8df8f90c3f614c904ed38e07beaa43bc96fa0
Sporesirius <38188453+Sporesirius@users.noreply.github.com>
2018-07-19 14:43:38 +0200

    Update teaspeak_install.sh
    Fixed packages for installer
    Added no login

--- teaspeak_install.sh
+++ teaspeak_install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 INSTALLER_VERSION="1.5"
-INSTALLER_REPO_URL="https://api.github.com/repos/Sporesirius/TeaSpeak-Installer/releases/latest"
-TEASPEAK_VERSION=$(curl -s -S -k https://repo.teaspeak.de/latest)
-REQUEST_URL="https://repo.teaspeak.de/server/linux/x64/TeaSpeak-${TEASPEAK_VERSION}.tar.gz"
 
 function whiteMessage {
     echo -e "\\033[0;37m${@}\033[0m"
@@ -72,69 +69,77 @@ function checkInstall {
 }
 
 cyanMessage " "
+cyanMessage " "
 redMessage "        TeaSpeak Installer"
 cyanMessage " "
 
-# We need to be root to install and update
+# We need to be root to run the installer.
 if [ "`id -u`" != "0" ]; then
     errorAndExit "Root account is required to run the install script!"
 fi
 
-cyanMessage "Checking for the latest installer version..."
-LATEST_VERSION=`wget -q --timeout=60 -O - ${INSTALLER_REPO_URL} | grep -Po '(?<="tag_name": ")([0-9]\.[0-9]+)'
-`
-GET_NEW_VERSION="https://github.com/Sporesirius/TeaSpeak-Installer/archive/${LATEST_VERSION}.tar.gz"
-
-if [ "`printf "${LATEST_VERSION}\n${INSTALLER_VERSION}" | sort -V | tail -n 1`" != "$INSTALLER_VERSION" ]; then
-    redWarnAndSleep "New version available. Downloading new installer version..."
-	wget ${GET_NEW_VERSION} -O installer_latest.tar.gz
-	greenOkAndSleep "DONE!"
-	
-	cyanMessage " "
-	greenOkAndSleep "# Unpacking installer and replace the old installer with the new one."
-	tar -xzf installer_latest.tar.gz
-	rm installer_latest.tar.gz
-	cd TeaSpeak-Installer-*
-	cp teaspeak_install.sh ../teaspeak_install.sh
-	cd ..
-	rm -R TeaSpeak-Installer-*
-	greenOkAndSleep "DONE!"
-	
-	cyanMessage " "
-	greenOkAndSleep "# Making new script executable."
-	chmod 774 teaspeak_install.sh
-	greenOkAndSleep "DONE!"
-	
-	cyanMessage " "
-	greenOkAndSleep "# Restarting script now"
-	clear
-	./teaspeak_install.sh
-	exit 0
-else
-    greenOkAndSleep "# You are using the up to date version ${INSTALLER_VERSION}."
-fi
-
-# Debian and its derivatives store their version at /etc/debian_version
+# Check if the operating system is Debian or its derivatives.
 if [ -f /etc/debian_version ]; then
-
-	cyanMessage " "
 	OS=`lsb_release -i 2> /dev/null | grep 'Distributor' | awk '{print($3)}'`
 	
+	cyanMessage " "
 	if [ "$OS" == "" ]; then
 		errorAndExit "Error: Could not detect OS. Currently only Debian and Ubuntu are supported.. Sry!"
 	else
 		greenOkAndSleep "$OS detected."
 	fi
+	
+	cyanMessage " "
+	greenOkAndSleep "# Installing necessary TeaSpeak-Installer packages..."
+	checkInstall wget
+	checkInstall curl
+	checkInstall tar
+	greenOkAndSleep "DONE!"
+	
+	cyanMessage " "
+	cyanMessage "Checking for the latest installer version..."
+	INSTALLER_REPO_URL="https://api.github.com/repos/Sporesirius/TeaSpeak-Installer/releases/latest"
+	LATEST_VERSION=`wget -q --timeout=60 -O - ${INSTALLER_REPO_URL} | grep -Po '(?<="tag_name": ")([0-9]\.[0-9]+)'`
+	TEASPEAK_VERSION=$(curl -s -S -k https://repo.teaspeak.de/latest)
+	REQUEST_URL="https://repo.teaspeak.de/server/linux/x64/TeaSpeak-${TEASPEAK_VERSION}.tar.gz"
+	GET_NEW_VERSION="https://github.com/Sporesirius/TeaSpeak-Installer/archive/${LATEST_VERSION}.tar.gz"
 
-    greenOkAndSleep "# Updating the system packages..."
+	if [ "`printf "${LATEST_VERSION}\n${INSTALLER_VERSION}" | sort -V | tail -n 1`" != "$INSTALLER_VERSION" ]; then
+		redWarnAndSleep "New version available. Downloading new installer version..."
+		wget ${GET_NEW_VERSION} -O installer_latest.tar.gz
+		greenOkAndSleep "DONE!"
+	
+		cyanMessage " "
+		greenOkAndSleep "# Unpacking installer and replace the old installer with the new one."
+		tar -xzf installer_latest.tar.gz
+		rm installer_latest.tar.gz
+		cd TeaSpeak-Installer-*
+		cp teaspeak_install.sh ../teaspeak_install.sh
+		cd ..
+		rm -R TeaSpeak-Installer-*
+		greenOkAndSleep "DONE!"
+	
+		cyanMessage " "
+		greenOkAndSleep "# Making new script executable."
+		chmod 774 teaspeak_install.sh
+		greenOkAndSleep "DONE!"
+
+		cyanMessage " "
+		greenOkAndSleep "# Restarting script now"
+		clear
+		./teaspeak_install.sh
+		exit 0
+	else
+		greenOkAndSleep "# You are using the up to date version ${INSTALLER_VERSION}."
+	fi
+
+	# Create User, install packages and download TeaSpeak.
+	greenOkAndSleep "# Updating the system packages..."
     apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
 	greenOkAndSleep "DONE!"
 	
 	cyanMessage " "
-	greenOkAndSleep "# Installing necessary TeaSpeak and Installer packages..."
-	checkInstall curl
-	checkInstall wget
-	checkInstall tar
+	greenOkAndSleep "# Installing necessary TeaSpeak packages..."
 	checkInstall screen
 	checkInstall ffmpeg
 	checkInstall youtube-dl
@@ -149,30 +154,28 @@ if [ -f /etc/debian_version ]; then
 	cyanMessage "Empty input = /home/ | Example input = /srv/"
 	read teaPath
 	
-	groupadd $teaUser
-	if [ "$teaPath" == "" ]; then
-		useradd -m -b /home -s /bin/bash -g $teaUser $teaUser
-		cd /home/$teaUser/
-	else
-		mkdir -p /$teaPath
-		useradd -m -b /$teaPath -s /bin/bash -g $teaUser $teaUser
-		cd /$teaPath/$teaUser/
-	fi
-	
     cyanMessage " "
-    cyanMessage "Create key or set password for login?"
-    cyanMessage "Safest way of login is a password protected key."
+    cyanMessage "Create key, set password or set non login?"
 	
-    OPTIONS=("Create key" "Set password" "Quit")
+    OPTIONS=("Create key" "Set password" "No Login" "Quit")
     select OPTION in "${OPTIONS[@]}"; do
         case "$REPLY" in
-            1|2 ) break;;
-            3 ) errorAndQuit;;
+            1|2|3 ) break;;
+            4 ) errorAndQuit;;
             *) errorAndContinue;;
         esac
     done
 
     if [ "$OPTION" == "Create key" ]; then
+		groupadd $teaUser
+		if [ "$teaPath" == "" ]; then
+			useradd -m -b /home -s /bin/bash -g $teaUser $teaUser
+			cd /home/$teaUser/
+		else
+			mkdir -p /$teaPath
+			useradd -m -b /$teaPath -s /bin/bash -g $teaUser $teaUser
+			cd /$teaPath/$teaUser/
+		fi
 
         if [ -d /home/$teaUser/.ssh ]; then
             rm -rf /home/$teaUser/.ssh
@@ -203,8 +206,37 @@ if [ -f /etc/debian_version ]; then
         fi
 
     elif [ "$OPTION" == "Set password" ]; then
+	
+		groupadd $teaUser
+		if [ "$teaPath" == "" ]; then
+			useradd -m -b /home -s /bin/bash -g $teaUser $teaUser
+			cd /home/$teaUser/
+		else
+			mkdir -p /$teaPath
+			useradd -m -b /$teaPath -s /bin/bash -g $teaUser $teaUser
+			cd /$teaPath/$teaUser/
+		fi
+		
         passwd $teaUser
+	elif [ "$OPTION" == "No Login" ]; then
+	
+		groupadd $teaUser
+		if [ "$teaPath" == "" ]; then
+			useradd -m -b /home -s /usr/sbin/nologin -g $teaUser $teaUser
+			cd /home/$teaUser/
+		else
+			mkdir -p /$teaPath
+			useradd -m -b /$teaPath -s /usr/sbin/nologin -g $teaUser $teaUser
+			cd /$teaPath/$teaUser/
+		fi
     fi
+	
+	
+	
+	
+	
+	
+	
 	
 	if [ "$teaPath" == "" ]; then
 		cd /home/$teaUser/
@@ -242,7 +274,6 @@ if [ -f /etc/debian_version ]; then
 
 	cyanMessage " "
 	greenOkAndSleep "Finished, TeaSpeak ${version} is now installed!"
-
 fi
 
 exit 0
